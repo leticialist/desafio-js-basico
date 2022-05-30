9542498f39cea8d0bace49d616d43686f2837960
Leticia Soares <leticiabgmr@gmail.com>
2022-05-30 17:16:15 -0300

    Desafio JS Básico

---
+++ index.js
@@ -0,0 +1,27 @@
+function somar(numero1, numero2) {
+    let soma = numero1 + numero2;
+    console.log ('A soma de ' + numero1 + ' + ' + numero2 + ' é igual a ' + soma)
+    }
+
+somar(1,3)
+
+function subtrair(numero1, numero2) {
+    let subtracao = numero1 - numero2;
+    console.log ('A subtração de ' + numero1 + ' - ' + numero2 + ' é igual a ' + subtracao)
+}    
+
+subtrair(9,4)
+
+function multiplicar(numero1, numero2) {
+    let multiplicacao = numero1 * numero2;
+    console.log('A multiplicação de ' + numero1 + ' * '+ numero2 + ' é igual a ' + multiplicacao)
+}
+
+multiplicar(5,6)
+
+function dividir(numero1, numero2) {
+    let divisao = numero1 / numero2;
+    console.log('A divisão de ' + numero1 + ' / ' + numero2 + ' é igual a ' + divisao)
+}
+
+dividir(7,14)
